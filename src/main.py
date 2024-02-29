a205ee2f920e2a7b586f254233a788804f307972
AshCena <atish.sinha99@gmail.com>
2024-02-28 22:46:38 -0500

    New ERC20 Modified code

--- src/main.py
+++ src/main.py
@@ -1,3 +1,117 @@
 import os
 import requests
 import json
+
+SCORE_SUCCESSFULL_TOKEN_BALANCE = 20
+SCORE_SUCCESSFULL_NFT_MINT = 30
+SCORE_SUCCESSFULL_TRASFER = 40
+
+feedbackRes = []
+
+def send_feedback(score, msg):
+    post = {'fractionalScore': score, 'feedback': msg}
+    # Optional: this goes to container log and is best practice for debugging purpose
+    print(json.dumps(post))
+    # This is required for actual feedback to be surfaced
+    with open(os.getenv('OUTPUT_PATH'), "w") as outfile:
+        json.dump(post, outfile)
+
+def get_tokenbalance_by_address(api_key, address, decentralised_id):
+    url = "https://api-sepolia.etherscan.io/api"
+    params = {
+        "module": "account",
+        "action": "tokenbalance",
+        "contractAddress": address,
+        "address": decentralised_id,
+        "tag": "latest",
+        "apikey": api_key
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+    
+def calculateTokenBalanceScore(transactions):
+    score = 0
+    trxs = json.loads(json.dumps(transactions))
+    if(int(trxs["result"]) <= 0):
+       feedbackRes.append("You lost 20 points for not meeting the TOKEN_BALANCE requirement. Please mkae sure that you (the owner) has sufficient token supply.")
+       return score
+    feedbackRes.append("You gained 20 points for meeting the TOKEN_BALANCE requirement.")
+    score += SCORE_SUCCESSFULL_TOKEN_BALANCE
+    for i in range(len(trxs["result"])):
+        del trxs["result"][i]["input"]
+    return score
+
+def calculateNftMintScore(transactions, contract_address, decentralized_id):
+    score = 0
+    trxs = json.loads(json.dumps(transactions))
+    if(len(trxs["result"]) < 1):
+       return score
+    for i in range(len(trxs["result"])):
+       curr = trxs["result"][i]
+       if(curr["contractAddress"].upper() == contract_address.upper() and curr["to"].upper() == decentralized_id.upper()):
+            feedbackRes.append("Successfully minted: 30 points.")
+            score += SCORE_SUCCESSFULL_NFT_MINT;
+            break
+    
+    if (score == 0):
+        feedbackRes.append("Mint function not implemented as per requirements. Refer to the handout. Lost 30 points for mint.")
+    return score
+
+def calculateTransferFromScore(transactions, from_address, benefeciary_id):
+    score = 0
+    trxs = json.loads(json.dumps(transactions))
+    if(len(trxs["result"]) < 1):
+       return score
+    for i in range(len(trxs["result"])):
+       curr = trxs["result"][i]
+       if(curr["from"].upper() == from_address.upper() and curr["to"].upper() == benefeciary_id.upper()):
+            feedbackRes.append("Successfully transfer: 40 points.")
+            score += SCORE_SUCCESSFULL_TRASFER;
+            break
+       
+    if(score == 0):
+        feedbackRes.append("Transfer function not implemented as per requirements. Refer to the handout. Lost 30 points for mint.")
+    return score
+
+def init(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    contract_address = decentralized_id = beneficiary_address = None
+    for line in lines:
+        if "contract_address" in line:
+            contract_address = line.split('=')[1].strip().replace('"', '')
+        elif "decentralized_id" in line:
+            decentralized_id = line.split('=')[1].strip().replace('"', '')
+        elif "beneficiary_address" in line:
+            beneficiary_address = line.split('=')[1].strip().replace('"', '')
+
+    if(beneficiary_address and decentralized_id and beneficiary_address):
+        return contract_address, decentralized_id, beneficiary_address
+    raise RuntimeError
+
+api_key = "3HZ5V116QT4B7TTASVGFH7A5EZCZGAJTKD"
+
+
+file_path = os.getenv('USER_FILE_PATH')
+if not file_path:
+    raise ValueError("Environment variable 'USER_FILE_PATH' not set")
+contract_address, decentralized_id, beneficiary_address = init(file_path)
+balance_transactions = get_tokenbalance_by_address(api_key, contract_address, decentralised_id=decentralized_id)
+
+
+if transactions:
+    scores = [
+        calculateTokenBalanceScore(balance_transactions),
+        calculateNftMintScore(transactions, contract_address, decentralized_id),
+        calculateTransferFromScore(transactions, decentralized_id, beneficiary_address)
+    ]
+
+    total = 0
+    for j in range(len(scores)):
+        total += scores[j]
+
+    separator = " "
+    send_feedback(total / 100, separator.join(feedbackRes))
--- src/test1.py
+++ src/test1.py
@@ -2,11 +2,12 @@ import os
 import requests
 import json
 
+SCORE_SUCCESSFULL_TOKEN_BALANCE = 20
 SCORE_SUCCESSFUL_MINT = 40
 SCORE_SUCCESSFUL_TRANSFER =40
-
+total = 0
 feedbackRes = []
-
+scores_map = {}
 def send_feedback(score, msg):
     post = {'fractionalScore': score, 'feedback': msg}
     # Optional: this goes to container log and is best practice for debugging purpose
@@ -16,8 +17,32 @@ def send_feedback(score, msg):
         json.dump(post, outfile)
 
 
-def get_transactions_by_address(contract_address, decentralized_id, api_key,beneficiary_address):
-    is_token_minted = is_transfered = False
+def get_tokenbalance_by_address(api_key, address, decentralised_id):
+    url = "https://api-sepolia.etherscan.io/api"
+    params = {
+        "module": "account",
+        "action": "tokenbalance",
+        "contractAddress": address,
+        "address": decentralised_id,
+        "tag": "latest",
+        "apikey": api_key
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+    
+def calculateTokenBalanceScore(transactions):
+    trxs = json.loads(json.dumps(transactions))
+    if(int(trxs["result"]) <= 0):
+       feedbackRes.append("You lost 20 points for not meeting the TOKEN_BALANCE requirement. Please make sure that you (the owner) has sufficient token supply.")
+       return
+    feedbackRes.append("You gained 20 points for meeting the TOKEN_BALANCE requirement.")
+    scores_map["TOKEN_BALANCE"] = SCORE_SUCCESSFULL_TOKEN_BALANCE
+
+def get_transactions_by_address(contract_address, decentralized_id, token_name, token_symbol, api_key):
+    
     url = "https://api-sepolia.etherscan.io/api"
     params = {
         "module": "account",
@@ -31,13 +56,22 @@ def get_transactions_by_address(contract_address, decentralized_id, api_key,bene
         "sort": "asc",
         "apikey": api_key
     }
-    response = requests.get(url, params=params)
-    temp  = response.json()
-    print("resp",temp)
+    try:
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+    except requests.exceptions.RequestException as err:
+        print(f'Error occurred during request: {err}')
+        return None
 
-    if response.status_code == 200:
-        for transaction in temp['result']:
-            print()
+def checkMintingAndTokenTransfer(transactions, decentralized_id ,beneficiary_address):
+    is_token_minted = is_transfered = False
+    print("transaction: ", transactions)
+    if transactions["message"] == "OK":
+        for transaction in transactions['result']:
+            print("ALL")
             print("Block Number:", transaction["blockNumber"])
             print("From:", transaction["from"])
             print("To:", transaction["to"])
@@ -47,27 +81,27 @@ def get_transactions_by_address(contract_address, decentralized_id, api_key,bene
                 mint_conditions = (
                     transaction["from"] == "0x0000000000000000000000000000000000000000",
                     transaction["to"].lower() == decentralized_id.lower(),
-                    transaction["tokenSymbol"].lower() == transaction["tokenSymbol"].lower(),
-                    transaction["tokenName"].lower() == transaction["tokenName"].lower()
+                    transaction["tokenSymbol"].lower() == token_symbol.lower(),
+                    transaction["tokenName"].lower() == token_name.lower()
                 )
                 if all(mint_conditions):
                     is_token_minted = True
 
             if not is_transfered:
                 result = float(transaction["value"])
-                token_value = result // 10 ** 18
-
-                print("Transferred Token Value:", token_value)
-                print("benef:", beneficiary_address.lower())
-                print("tran:", transaction["to"].lower())
-                print("res",transaction["to"].lower() == beneficiary_address.lower())
+                token_value = result
+                # print("TRANSFER")
+                # print("Transferred Token Value:", token_value)
+                # print("benef:", beneficiary_address.lower())
+                # print("tran:", transaction["to"].lower())
+                # print("res",transaction["to"].lower() == beneficiary_address.lower())
 
 
                 transfer_conditions = (
                     transaction["from"].lower() == decentralized_id.lower(),
                     transaction["to"].lower() == beneficiary_address.lower(),
-                    transaction["tokenSymbol"].lower() == transaction["tokenSymbol"].lower(),
-                    transaction["tokenName"].lower() == transaction["tokenName"].lower(),
+                    transaction["tokenSymbol"].lower() == token_symbol.lower(),
+                    transaction["tokenName"].lower() == token_name.lower(),
                     token_value > 0
                 )
                 if all(transfer_conditions):
@@ -76,97 +110,34 @@ def get_transactions_by_address(contract_address, decentralized_id, api_key,bene
             if is_token_minted and is_transfered:
                 break
 
-        print("Is Mint Operation performed on ERC20 Token : " + str(is_token_minted));
-        print("Is Transaction Operation performed on ERC20 Token : " + str(is_transfered));
+        print("Is Mint Operation performed on ERC20 Token : " + str(is_token_minted))
+        print("Is Transaction Operation performed on ERC20 Token : " + str(is_transfered))
 
-        score = 0
-        map = {}
 
         if (is_token_minted):
-            score += 40;
-            map["MINT_TOKEN"]= 40
-            if (is_transfered):
-                score += 40
-                map["TOKEN_TRANSFER"]=40
+            scores_map["MINT_TOKEN"] = SCORE_SUCCESSFUL_MINT
+            feedbackRes.append("You gained 40 points for meeting mint token requirement.")
+        else:
+            feedbackRes.append("You lost 40 points for not meeting mint token requirement.")
 
-                print("Map [Score : Transfer and Mint] : " + map);
+        if (is_transfered):
+            scores_map["TOKEN_TRANSFER"] = SCORE_SUCCESSFUL_TRANSFER
+            feedbackRes.append("You gained 40 points for meeting transfer token requirement.")
+        else:
+            feedbackRes.append("Transfer function not implemented as per requirements. Refer to the handout. Lost 40 points for Transfer Token.")
 
-                return map
-        return response.json()
+        print("scores_map [Score : Transfer and Mint] : " + str(scores_map.values()))
+
     else:
-        return None
-		
-			
-
-def isMinted(contract_address: str, decentralized_id: str, api_key: str) -> int:
-    """Function to check if the token is minted."""
-    url = "https://api-sepolia.etherscan.io/api"
-
-    params = {
-        "module": "account",
-        "action": "tokentx",
-        "contractAddress": contract_address,
-        "address":decentralized_id,
-        "page": 1,
-        "offset":100,
-        "startblock": 0,
-        "endblock": 99999999,
-        "sort": "asc",
-        "apikey": api_key
-    }
-    try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()  # Raise an HTTPError for bad status codes
-        transactions = response.json()
-
-        print("Status Code:", response.status_code)
-        print("Status Code:", transactions)
-        minted = any(transaction["contractAddress"].lower() == contract_address.lower() and 
-                     transaction["to"].lower() == decentralized_id.lower() for transaction in transactions.get("result", []))
-        if minted:
-            return SCORE_SUCCESSFUL_MINT
-        else:
-            return 0
-    except requests.exceptions.RequestException as err:
-        print(f'Error occurred during request: {err}')
-        return 0
-
-def isTransferred(decentralized_id: str, beneficiary_address: str, api_key: str) -> int:
-    """Function to check if the token is transferred."""
-    url = "https://api-sepolia.etherscan.io/api"
-    params = {
-        "module": "account",
-        "action": "tokennfttx",
-        "contractAddress": contract_address,
-        "startblock": 0,
-        "endblock": 99999999,
-        "sort": "asc",
-        "apikey": api_key
-    }
-    try:
-        response = requests.get(url, params=params)
-        print("Status Code:", response.status_code)
-        response.raise_for_status()  # Raise an HTTPError for bad status codes
-        print("y")
-        transactions = response.json()
-        print("here")
-        print("tx", transactions)
-        transferred = any(transaction["from"].lower() == decentralized_id.lower() and
-                         transaction["to"].lower() == beneficiary_address.lower() for transaction in transactions.get("result", []))
-        if transferred:
-            return SCORE_SUCCESSFUL_TRANSFER
-        else:
-            return 0
-    except requests.exceptions.RequestException as err:
-        print(f'Error occurred during request: {err}')
-        return 0
+        feedbackRes.append("You lost 40 points for not meeting mint token requirement.")
+        feedbackRes.append("Transfer function not implemented as per requirements. Refer to the handout. Lost 40 points for Transfer Token.")
 
 
 
 def init(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
-    contract_address = decentralized_id = beneficiary_address = None
+    contract_address = decentralized_id = beneficiary_address = token_name = token_symbol = None
     for line in lines:
         if "contract_address" in line:
             contract_address = line.split('=')[1].strip().replace('"', '')
@@ -174,33 +145,48 @@ def init(file_path):
             decentralized_id = line.split('=')[1].strip().replace('"', '')
         elif "beneficiary_address" in line:
             beneficiary_address = line.split('=')[1].strip().replace('"', '')
+        elif "token_name" in line:
+            token_name = line.split('=')[1].strip().replace('"', '')            
+        elif "token_symbol" in line:
+            token_symbol = line.split('=')[1].strip().replace('"', '')
 
-    if(beneficiary_address and decentralized_id and beneficiary_address):
-        return contract_address, decentralized_id, beneficiary_address
+
+    if(beneficiary_address and decentralized_id and beneficiary_address and token_name and token_symbol):
+        return contract_address, decentralized_id, beneficiary_address, token_name, token_symbol
     raise RuntimeError
 
 api_key = "4WT5YN9UASF77UJ2ZV5KF25B8IKES3C1K5"
 SEPOLIA_URL = "https://api-sepolia.etherscan.io/api?"
+initializations = 1
+alphanumcheck = 1
+try:
+    file_path = os.getenv('USER_FILE_PATH')
+    if not file_path:
+        raise ValueError("Environment variable 'USER_FILE_PATH' not set")
 
+    contract_address, decentralized_id, beneficiary_address, token_name, token_symbol = init(file_path)
+    balance_transactions = get_tokenbalance_by_address(api_key, contract_address, decentralised_id=decentralized_id)
+    transactions = get_transactions_by_address(contract_address, decentralized_id, token_name, token_symbol, api_key)
+except:
+    initializations = 0
+    feedbackRes.append("Error while grading! Please contact the support!")
 
-#file_path = os.getenv('USER_FILE_PATH')
+if  initializations and not (contract_address.isalnum() and decentralized_id.isalnum() and beneficiary_address.isalnum()):
+    alphanumcheck = 0
+    feedbackRes.append("Only alphaneumeric values are valid for token/contract/wallet addresses.")
+    
 
-contract_address, decentralized_id, beneficiary_address ="0xb1AC9E086381e22adB6B36A211c3517f18CAc200","0x1dF4E224E016DB06e25d27120c0BD333617E6Fc3","0x6aa85B820ea3fA061CA5831DcD47De7Bc888324C"
-
-transactions = get_transactions_by_address(contract_address, decentralized_id,api_key ,beneficiary_address)
-#isMinted(transactions, contract_address, decentralized_id),
-print(transactions)
-if not transactions:
+if initializations and alphanumcheck and transactions and balance_transactions:
+    calculateTokenBalanceScore(balance_transactions)
+    checkMintingAndTokenTransfer(transactions, decentralized_id ,beneficiary_address)
     scores = [
-        
-        isMinted(contract_address, decentralized_id,api_key),
-		isTransferred(transactions, contract_address, decentralized_id)
-        
+       scores_map.get("TOKEN_BALANCE", 0),
+       scores_map.get("MINT_TOKEN", 0),
+       scores_map.get("TOKEN_TRANSFER", 0)
     ]
 
-    total = 0
-    for j in range(len(scores)):
-        total += scores[j]
+    for key, val in scores_map.items():
+        total += val
 
-    separator = " "
-    send_feedback(total / 100, separator.join(feedbackRes))
+separator = " "
+send_feedback(total / 100, separator.join(feedbackRes))
